95ec5fae33e3b0bf5dd4386148bea16603c3673f
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-11-26 14:18:52 +0000

    Added VAR/TMP nosuid check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -238,6 +238,19 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "/var/tmp nosuid"
+check_description = "-"
+command = "sudo findmnt -n /var/tmp | grep -c -Ev '\\bnosuid\\b' || true"
+run_command = subprocess.check_output(command, shell=True)
+vartmp_nosuid_check = run_command.decode("utf-8")
+
+if re.match("0", vartmp_nosuid_check):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
