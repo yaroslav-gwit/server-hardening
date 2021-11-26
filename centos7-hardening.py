7c95ff7cbe15871e86f36864dc66ce7a93c8f451
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-11-26 15:52:54 +0000

    Added /home partition check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -279,6 +279,20 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+### LVL2 Check ###
+check_name = "/home partition check"
+check_description = "-"
+command = "sudo findmnt /home | wc -l"
+run_command = subprocess.check_output(command, shell=True)
+home_partition_check = run_command.decode("utf-8")
+
+if re.match("[^0]", home_partition_check):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl2_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
