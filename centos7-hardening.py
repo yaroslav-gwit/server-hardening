5d149339e7f9c1b6d817c115553e851f3faa8708
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-11-26 13:13:19 +0000

    Typo fix

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -210,4 +210,4 @@ else:
 
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
-print(bloded_string_TotalScore + ": " + total_score)
+print(bloded_string_TotalScore + ": " + str(total_score))
