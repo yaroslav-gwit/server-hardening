5313fd73b76d93efa4d0983a857c7cb5dbccf9c9
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-11-29 15:42:14 +0000

    Added auth for single user mode check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -463,6 +463,23 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "Authentication for single user mode"
+check_description = "-"
+command = "sudo grep /sbin/sulogin /usr/lib/systemd/system/rescue.service"
+run_command = subprocess.check_output(command, shell=True)
+auth_for_single_user_mode_1 = run_command.decode("utf-8")
+
+command = "sudo grep /sbin/sulogin /usr/lib/systemd/system/emergency.service"
+run_command = subprocess.check_output(command, shell=True)
+auth_for_single_user_mode_2 = run_command.decode("utf-8")
+
+if re.match("700", auth_for_single_user_mode_1 and auth_for_single_user_mode_2):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
