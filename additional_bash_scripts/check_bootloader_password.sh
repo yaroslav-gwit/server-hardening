9858d2e171e46a8510fe09fc9a382b94061d65ad
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-11-29 14:45:14 +0000

    fixed typos

--- additional_bash_scripts/check_bootloader_password.sh
+++ additional_bash_scripts/check_bootloader_password.sh
@@ -11,4 +11,6 @@ elif [ -f "$gbdir"/grub.cfg ]; then grubdir="$gbdir" && grubfile="$gbdir/grub.cf
 fi
 
 userfile="$grubdir/user.cfg"
-[ -f "$userfile" ] && grep -Pq '^\h*GRUB2_PASSWORD\h*=\h*.+$' "$userfile" && output="\n PASSED: bootloader password set in \"$userfile\"\n\n" if [ -z "$output" ] && [ -f "$grubfile" ]; then grep -Piq '^\h*set\h+superusers\h*=\h*"?[^"\n\r]+"?(\h+.*)?$' "$grubfile" && tst1=pass grep -Piq '^\h*password\h+\H+\h+.+$' "$grubfile" && tst2=pass [ "$tst1" = pass ] && [ "$tst2" = pass ] && output="\n\n*** PASSED: bootloader password set in \"$grubfile\" ***\n\n" fi [ -n "$output" ] && echo -e "$output" || echo -e "\n\n *** FAILED: bootloader password is not set ***\n\n"
+[ -f "$userfile" ] && grep -Pq '^\h*GRUB2_PASSWORD\h*=\h*.+$' "$userfile" && output="\n PASSED: bootloader password set in \"$userfile\"\n\n"
+if [ -z "$output" ] && [ -f "$grubfile" ]; then grep -Piq '^\h*set\h+superusers\h*=\h*"?[^"\n\r]+"?(\h+.*)?$' "$grubfile" && tst1=pass grep -Piq '^\h*password\h+\H+\h+.+$' "$grubfile" && tst2=pass [ "$tst1" = pass ] && [ "$tst2" = pass ] && output="\n\n*** PASSED: bootloader password set in \"$grubfile\" ***\n\n" fi [ -n "$output" ] && echo -e "$output" || echo -e "\n\n *** FAILED: bootloader password is not set ***\n\n"
+fi
