0aa9da4cb8e5edbb0cf3a860403db83329900b1e
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-11-30 15:28:22 +0000

    Added Remote login warning banner check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -684,6 +684,19 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "Remote login warning banner is configured"
+check_description = "-"
+command = "sudo cat /etc/issue.net"
+run_command = subprocess.check_output(command, shell=True)
+etc_issue_net_is_configured = run_command.decode("utf-8")
+
+if re.match("Authorized users only. All activity may be monitored and reported.", etc_issue_net_is_configured):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
