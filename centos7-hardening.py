5572d203f7620b3cbf224a7a5482eeafed2f9633
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-11-30 14:35:04 +0000

    Added SELinux policy is configured check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -584,6 +584,20 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "SELinux policy is configured"
+check_description = "-"
+
+command = "sudo grep -G \"^SELINUXTYPE=\" /etc/selinux/config || true"
+run_command = subprocess.check_output(command, shell=True)
+selinux_policy_is_configured = run_command.decode("utf-8")
+
+if re.match("SELINUXTYPE=targeted", selinux_policy_is_configured):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
