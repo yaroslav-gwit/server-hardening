e1e643ffdf2f5264d86ed5ff9279d6bebeb54883
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-11-30 14:31:17 +0000

    Added SELinux is not disabled during boot check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -570,6 +570,20 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "SELinux is not disabled during boot"
+check_description = "-"
+
+command = "sudo grep -Eq \"(selinux=0|enforcing=0)\" /boot/efi/EFI/centos/grub.cfg | wc -l || true"
+run_command = subprocess.check_output(command, shell=True)
+selinux_is_not_disabled_in_grubcfg = run_command.decode("utf-8")
+
+if re.match("0", selinux_is_not_disabled_in_grubcfg):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
