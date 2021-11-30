1d8c39797e898cfb0b7797d4579967461aa05b6b
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-11-30 12:40:09 +0000

    Fixed restricted_core_dumps_4 subcheck typos

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -501,7 +501,7 @@ restricted_core_dumps_3_regex = "fs.suid_dumpable = 0"
 command = "sudo systemctl is-enabled coredump.service 2> /dev/null | wc -l || true"
 run_command = subprocess.check_output(command, shell=True)
 restricted_core_dumps_4 = run_command.decode("utf-8")
-restricted_core_dumps_4_regex = "No such file or directory"
+restricted_core_dumps_4_regex = "0"
 
 if re.match(restricted_core_dumps_1_regex, restricted_core_dumps_1) and re.match(restricted_core_dumps_2_regex, restricted_core_dumps_2) and re.match(restricted_core_dumps_3_regex, restricted_core_dumps_3) and re.match(restricted_core_dumps_4_regex, restricted_core_dumps_4):
     task_list.append([check_name, Passed, check_description])
