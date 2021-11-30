3aa11cbbbc38cf28c75da98129b3cb7d18717a91
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-11-30 15:01:37 +0000

    Added SETroubleshoot is not installed check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -630,6 +630,20 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "SETroubleshoot is not installed"
+check_description = "-"
+
+command = "sudo rpm -q setroubleshoot || true"
+run_command = subprocess.check_output(command, shell=True)
+setroubleshoot_is_not_installed = run_command.decode("utf-8")
+
+if re.match("package setroubleshoot is not installed", setroubleshoot_is_not_installed):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
