134d28ab5b9fba59190d6de555fcd709893a0af4
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-12-01 16:09:52 +0000

    Added ip forwarding disabled check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -1120,6 +1120,20 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "IP forwarding is disabled"
+check_description = "-"
+
+command = "sudo sysctl net.ipv4.ip_forward"
+run_command = subprocess.check_output(command, shell=True)
+ip_forwarding_disabled = run_command.decode("utf-8")
+
+if re.match("net.ipv4.ip_forward = 0", ip_forwarding_disabled):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
