2f49be6658b1f9d7a4e997bd1434b1431ce28a57
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-12-01 12:42:17 +0000

    Added MTA is configured for local-only mode check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -954,6 +954,20 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "MTA is configured for local-only mode"
+check_description = "-"
+
+command = "sudo ss -lntu | grep -E ':25\s' | grep -E -v '\s(127.0.0.1|\[?::1\]?):25\s' | wc -l"
+run_command = subprocess.check_output(command, shell=True)
+mta_local_only_mode = run_command.decode("utf-8")
+
+if re.match("0", mta_local_only_mode):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
