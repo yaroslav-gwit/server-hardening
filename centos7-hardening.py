1ad0b1793ab0ad88d89ca6593af3e8c9457fc7ba
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-12-01 11:38:29 +0000

    Added DNS server is not installed check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -828,6 +828,20 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "DNS server is not installed"
+check_description = "-"
+
+command = "sudo rpm -q bind || true"
+run_command = subprocess.check_output(command, shell=True)
+dns_server_is_not_installed = run_command.decode("utf-8")
+
+if re.match("package bind is not installed", dns_server_is_not_installed):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
