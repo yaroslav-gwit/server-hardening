cf4c72cd63b95490fa602427bbbe3a4a2691f31e
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-12-02 12:02:48 +0000

    Added iptables-services not installed with firewalld check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -1302,6 +1302,20 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "iptables-services not installed with firewalld"
+check_description = "-"
+
+command = "sudo rpm -q iptables-services 2> /dev/null || true"
+run_command = subprocess.check_output(command, shell=True)
+iptables_services_not_installed = run_command.decode("utf-8")
+
+if re.match("package iptables-services is not installed", iptables_services_not_installed):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
