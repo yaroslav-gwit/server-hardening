1b22a640a9fc61d879c2da974798a9ef9b4ab891
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-12-02 12:04:55 +0000

    Added nftables not installed with firewalld check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -1316,6 +1316,20 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "nftables not installed with firewalld"
+check_description = "-"
+
+command = "sudo rpm -q nftables 2> /dev/null || true"
+run_command = subprocess.check_output(command, shell=True)
+nftables_not_installed = run_command.decode("utf-8")
+
+if re.match("package nftables is not installed", nftables_not_installed):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
