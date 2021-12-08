dc23186308197bad8e5dbf4524b7b5cde003af03
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-12-08 15:59:03 +0000

    Added firewalld drops unnecessary services and ports check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -1376,6 +1376,20 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "firewalld drops unnecessary services and ports"
+check_description = "Page 269: Ensure firewalld drops unnecessary services and ports"
+
+command = "sudo firewall-cmd --list-all --zone=public | grep services"
+run_command = subprocess.check_output(command, shell=True)
+interface_zone_assigned = run_command.decode("utf-8")
+
+if re.match("\s*services:\s*dhcpv6-client\s*ssh", interface_zone_assigned):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
