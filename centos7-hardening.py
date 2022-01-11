2944f8b7988ab6853e75e3d4184354e78b17e08f
Yaroslav Koisa <yaroslav.koisa@icr.ac.uk>
2022-01-11 16:14:14 +0000

    Added rsyslog_remote_server check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -1486,6 +1486,20 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "rsyslog is configured to send logs to a remote log host"
+check_description = "Page 403: 4.2.1.5 Ensure rsyslog is configured to send logs to a remote log host"
+
+command = "sudo grep \"authpriv.*\" /etc/rsyslog.conf 2>/dev/null || true"
+run_command = subprocess.check_output(command, shell=True)
+rsyslog_remote_server = run_command.decode("utf-8")
+
+if re.match("^authpriv.* @192.168.214.62", rsyslog_remote_server) or re.match("^authpriv.* @192.168.213.40", rsyslog_remote_server):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
