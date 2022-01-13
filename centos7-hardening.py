425595f493e60acdbb8bef6fb8cff2861f42bd82
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-01-13 11:40:58 +0000

    Added ssh_log_level check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -1811,6 +1811,26 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "Page 458: Ensure SSH LogLevel is appropriate"
+check_description = "-"
+
+command = "sudo sshd -T -C user=root -C host=\"$(hostname)\" -C addr=\"$(grep $(hostname) /etc/hosts | awk '{print $1}')\" | grep loglevel"
+run_command = subprocess.check_output(command, shell=True)
+ssh_log_level_1 = run_command.decode("utf-8")
+
+command = "sudo grep -i 'loglevel' /etc/ssh/sshd_config | grep -Ei '(VERBOSE|INFO)'"
+run_command = subprocess.check_output(command, shell=True)
+ssh_log_level_2 = run_command.decode("utf-8")
+
+ssh_log_level_re = ".*VERBOSE|.*INFO"
+
+if re.match(ssh_log_level_re, ssh_log_level_1) and re.match(ssh_log_level_re, ssh_log_level_2):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
