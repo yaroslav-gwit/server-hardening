c8d2296a369f490f247f6f80c33641f0025ce5f8
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-01-13 13:17:44 +0000

    Added ssh_permit_user_environment check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -1929,6 +1929,26 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "Page 472: Ensure SSH PermitUserEnvironment is disabled"
+check_description = "-"
+
+command = "sudo sshd -T -C user=root -C host=\"$(hostname)\" -C addr=\"$(grep $(hostname) /etc/hosts | awk '{print $1}')\" | grep permituserenvironment"
+run_command = subprocess.check_output(command, shell=True)
+ssh_permit_user_environment_1 = run_command.decode("utf-8")
+
+command = "sudo grep -Gi \"^permitemptypasswords\" /etc/ssh/sshd_config 2>/dev/null || true"
+run_command = subprocess.check_output(command, shell=True)
+ssh_permit_user_environment_2 = run_command.decode("utf-8")
+
+ssh_permit_user_environment_re = "[Pp]ermit[Uu]ser[Ee]nvironment no"
+
+if re.match(ssh_permit_user_environment_re, ssh_permit_user_environment_1) and re.match(ssh_permit_user_environment_re, ssh_permit_user_environment_2):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
