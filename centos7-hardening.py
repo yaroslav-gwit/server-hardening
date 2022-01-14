83d3eb2fd80c796721e309aa2c281a8d70e0d3e8
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-01-14 14:29:45 +0000

    Added inactive_password_lock check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -2213,6 +2213,22 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "Page 521: Ensure inactive password lock is 30 days or less"
+check_description = "-"
+
+command = "sudo useradd -D | grep INACTIVE"
+run_command = subprocess.check_output(command, shell=True)
+inactive_password_lock = run_command.decode("utf-8")
+
+inactive_password_lock_re = "INACTIVE=365"
+
+if re.match(inactive_password_lock_re, inactive_password_lock):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
