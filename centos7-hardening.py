4161c30c11a35de5e8b3d269f7187e4e634225d2
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-01-14 12:53:25 +0000

    Added limited_password_reuse check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -2155,6 +2155,22 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "Page 511: Ensure password reuse is limited"
+check_description = "-"
+
+command = "sudo grep pwhistory /etc/pam.d/system-auth /etc/pam.d/password-auth 2>/dev/null || true"
+run_command = subprocess.check_output(command, shell=True)
+limited_password_reuse = run_command.decode("utf-8")
+
+limited_password_reuse_re = ".*remember=10\s.*remember=10"
+
+if re.match(limited_password_reuse_re, limited_password_reuse):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
