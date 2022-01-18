c2138699952294e6f96f29ab7246109a062c4202
Yaroslav Koisa <yaroslav.koisa@icr.ac.uk>
2022-01-18 16:16:50 +0000

    Test

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -2295,6 +2295,21 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "Page 531: Ensure default user umask is configur"
+check_description = "-"
+
+command = "sudo " + bash_scripts_location + "check_default_user_umask.sh"
+run_command = subprocess.check_output(command, shell=True, stderr=DEVNULL)
+check_usr_shell_timeout = run_command.decode("utf-8")
+check_usr_shell_timeout_re = "1"
+
+if re.match(root_account_gid_is_0_re, root_account_gid_is_0):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
