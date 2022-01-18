e363a43429b729cb0b26cc8f30b5daf1602fd651
Yaroslav Koisa <yaroslav.koisa@icr.ac.uk>
2022-01-18 15:58:56 +0000

    Added check_usr_shell_timeout check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -2280,6 +2280,21 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "Page 528: Ensure default user shell timeout is configured"
+check_description = "-"
+
+command = "sudo " + bash_scripts_location + "check_usr_shell_timeout.sh"
+run_command = subprocess.check_output(command, shell=True, stderr=DEVNULL)
+check_usr_shell_timeout = run_command.decode("utf-8")
+check_usr_shell_timeout_re = "0"
+
+if re.match(root_account_gid_is_0_re, root_account_gid_is_0):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
