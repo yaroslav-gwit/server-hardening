1e8d98583b3573b9ae29aa8e1567037528ac4324
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-01-19 12:14:19 +0000

    Added etc_password_permissons check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -2324,6 +2324,21 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "Page 545: Ensure permissions on /etc/passwd are configured"
+check_description = "-"
+
+command = "stat /etc/passwd | grep \"Access: (\""
+run_command = subprocess.check_output(command, shell=True, stderr=DEVNULL)
+etc_password_permissons = run_command.decode("utf-8")
+etc_password_permissons_re = ".*0644.*root.*root"
+
+if re.match(etc_password_permissons_re, etc_password_permissons):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
