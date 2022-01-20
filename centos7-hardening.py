176b78b4760e24327597ae540e61380ba39f3836
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-01-20 16:20:35 +0000

    Added ensure_root_path_integrity check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -2644,6 +2644,21 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "Page 589: Ensure root PATH Integrity"
+check_description = "-"
+
+command = "bash " + bash_scripts_location + "check_root_path_integrity.sh | grep -v \"root's path\" | grep -v \"is not a directory\" | wc -l"
+run_command = subprocess.check_output(command, shell=True, stderr=DEVNULL)
+ensure_root_path_integrity = run_command.decode("utf-8")
+ensure_root_path_integrity_re = "0"
+
+if re.match(ensure_root_path_integrity_re, ensure_root_path_integrity):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
