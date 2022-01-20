f42551adc95affa53f868f9c791a335c6a98dfe4
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-01-20 15:58:11 +0000

    Added no_duplicate_users_exist check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -2569,6 +2569,21 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "Page 580: Ensure no duplicate user names exist"
+check_description = "-"
+
+command = "bash " + bash_scripts_location + "check_no_duplicate_usernames.sh | wc -l"
+run_command = subprocess.check_output(command, shell=True, stderr=DEVNULL)
+no_duplicate_users_exist = run_command.decode("utf-8")
+no_duplicate_users_exist_re = "0"
+
+if re.match(no_duplicate_users_exist_re, no_duplicate_users_exist):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
