26376561e714faa6ce12217bd32aff5ff3dde3a8
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-01-20 16:02:03 +0000

    Added no_duplicate_groups_exist check

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -2584,6 +2584,21 @@ else:
     task_list.append([check_name, Failed, check_description])
 
 
+check_name = "Page 582: Ensure no duplicate groups exist"
+check_description = "-"
+
+command = "bash " + bash_scripts_location + "check_no_duplicate_groups.sh | wc -l"
+run_command = subprocess.check_output(command, shell=True, stderr=DEVNULL)
+no_duplicate_groups_exist = run_command.decode("utf-8")
+no_duplicate_groups_exist_re = "0"
+
+if re.match(no_duplicate_groups_exist_re, no_duplicate_groups_exist):
+    task_list.append([check_name, Passed, check_description])
+    total_score = total_score + lvl1_plus
+else:
+    task_list.append([check_name, Failed, check_description])
+
+
 # Table printout #
 print(tabulate(task_list, table_headers, tablefmt="fancy_grid", showindex=range(1, len(task_list) + 1) ) )
 print(bloded_string_TotalScore + ": " + str(total_score))
