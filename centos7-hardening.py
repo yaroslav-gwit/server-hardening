0a099e2c32d87db471791346ba10daac7abd1e56
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-01-25 15:23:48 +0000

    Added few comments

--- centos7-hardening.py
+++ centos7-hardening.py
@@ -16,6 +16,7 @@ bolded_string_Status = "\033[1m" + "Status" + "\033[0m"
 bolded_string_Details = "\033[1m" + "Details" + "\033[0m"
 bloded_string_TotalScore = "\033[1m" + "Total System Hardening Points" + "\033[0m"
 
+# Score values
 total_score = 0
 lvl1_plus = 1
 lvl2_plus = 1
